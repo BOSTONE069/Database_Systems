83851c8015b18b9ed9ae8ffb5bac2a86452dee6a
rhemney <bostoneochieng69@gmail.com>
2022-03-29 04:18:57 -0700

    front end initialization

---
+++ frontend.py
@@ -0,0 +1,21 @@
+from tkinter import *
+
+win = Tk()
+
+l1 = Label(win, text='Date')
+l1.grid(row=0,column=0)
+l1 = Label(win, text='Date')
+l1.grid(row=0,column=0)
+l1 = Label(win, text='Date')
+l1.grid(row=0,column=0)
+l1 = Label(win, text='Date')
+l1.grid(row=0,column=0)
+l1 = Label(win, text='Date')
+l1.grid(row=0,column=0)
+l1 = Label(win, text='Date')
+l1.grid(row=0,column=0)
+
+
+
+
+win.mainloop()
